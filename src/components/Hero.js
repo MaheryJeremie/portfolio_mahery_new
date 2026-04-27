e253d2f073a0bf99eb8d1f51cc5bbffc26ef88e9
MaheryJeremie <mramahay2103@gmail.com>
2026-04-27 16:49:37 +0300

    fix: remove invisible characters from Hero.js import statement

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,4 @@
-﻿import React, { Suspense } from 'react';
+import React, { Suspense } from 'react';
 import { motion } from 'framer-motion';
 import { useLanguage } from '../contexts/LanguageContext';
 import HeroScene from './three/HeroScene';
